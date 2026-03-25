a2db0d45ad899dd18fdb91e2fd0f0be2a66739fe
Gaston Giorgio <gastongiorgio26@gmail.com>
2026-03-25 11:23:15 -0300

    Primer commit: Maquetación HTML, CSS y lógica JS del frontend

---
+++ frontend/js/data.js
@@ -0,0 +1,36 @@
+const categorias = ["Hamburguesas", "Pizzas", "Papas Fritas", "Bebidas"];
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Hamburguesa Triple",
+        descripcion: "Triple carne, cheddar y bacon",
+        precio: 25000,
+        imagen: "./assets/img/hamburguesa-triple.png",
+        categoria: "Hamburguesas"
+    },
+    {
+        id: 2,
+        nombre: "Pizza Muzzarella",
+        descripcion: "Salsa casera y orégano",
+        precio: 18000,
+        imagen: "./assets/img/pizza-muzzarella.png",
+        categoria: "Pizzas"
+    },
+    {
+        id: 3,
+        nombre: "Papas Fritas",
+        descripcion: "Papas Fritas con extra cheddar",
+        precio: 8000,
+        imagen: "./assets/img/papas-cheddar.png",
+        categoria: "Papas Fritas"
+    },
+    {
+        id: 4,
+        nombre: "Bebida",
+        descripcion: "Bebida fresca",
+        precio: 6000,
+        imagen: "./assets/img/bebida.png",
+        categoria: "Bebidas"
+    },
+]
